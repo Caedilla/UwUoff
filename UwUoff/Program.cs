8ab60a1746006a1f6c1b34d2d5a010198e2c04d3
Raeli <42666479+Caedilla@users.noreply.github.com>
2021-09-28 01:48:10 +0200

    first commit

---
+++ UwUoff/Program.cs
@@ -0,0 +1,32 @@
+﻿using DSharpPlus;
+using System.Configuration;
+using System.Threading.Tasks;
+
+namespace UwUoff
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MainAsync().GetAwaiter().GetResult();
+        }
+
+        static async Task MainAsync()
+        {
+            var discord = new DiscordClient(new DiscordConfiguration()
+            {
+                Token = ConfigurationManager.AppSettings["DISCORD_KEY"],
+                TokenType = TokenType.Bot
+            });
+
+            discord.MessageCreated += async (s, e) =>
+            {
+                if (e.Message.Content.ToLower().StartsWith("test"))
+                    await e.Message.RespondAsync("response");
+            };
+
+            await discord.ConnectAsync();
+            await Task.Delay(-1);
+        }
+    }
+}
