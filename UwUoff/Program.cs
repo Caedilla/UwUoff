fac3352761ccf9409655b451fc897e9f278e4a48
Raeli <42666479+Caedilla@users.noreply.github.com>
2021-09-28 02:58:52 +0200

    add reaction, don't respond to my own messages

--- UwUoff/Program.cs
+++ UwUoff/Program.cs
@@ -1,11 +1,14 @@
 ﻿using DSharpPlus;
 using System.Configuration;
 using System.Threading.Tasks;
+using DSharpPlus.Entities;
 
 namespace UwUoff
 {
     class Program
     {
+        private const long myID = 887375280451223582;
+
         static void Main(string[] args)
         {
             MainAsync().GetAwaiter().GetResult();
@@ -21,10 +24,17 @@ namespace UwUoff
 
             discord.MessageCreated += async (s, e) =>
             {
-                if (e.Message.Content.ToLower().StartsWith("test"))
-                    await e.Message.RespondAsync("response");
-            };
+                if (e.Message.Content.ToLower().StartsWith("test") && e.Message.Author.Id != myID)
+                    await e.Message.RespondAsync("test response");
 
+                bool isMe = e.Message.Author.Id == myID;
+                var joyEmoji = DiscordEmoji.FromUnicode("😂");
+
+                if (isMe)
+                    await e.Message.CreateReactionAsync(joyEmoji);
+            };
+            
+        
             await discord.ConnectAsync();
             await Task.Delay(-1);
         }
